c28a75106f25bd3898d6ddc843ce1219691de779
michelssousa <michelsousa7@gmail.com>
2021-02-16 09:35:53 -0300

    start

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import { Testando } from '@src/outro';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const {url} = process.env;
 
 const tt = new Testando('michel');
-console.log(`${tt.getTeste()}`)
-    
+console.log(`${tt.getTeste()} /n ${url}`)
+
---
+++ system-environment-variables.ts
@@ -0,0 +1,8 @@
+
+export const environmentVariables = {
+  url: 'http://localhost',
+  bdUrl: 'http://localhost',
+  jwt_secret_key: 'michel',
+  tokenExpiresIn: 200000,
+  port: 8080
+}
